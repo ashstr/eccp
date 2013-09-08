4ce6227ff16273f3461530cd13c31da13f94362d
unknown <str@.(none)>
2013-09-08 02:13:04 +0200

    Creating timerCount php file

---
+++ eccp-members/examTimeCheker.php
@@ -0,0 +1,13 @@
+<?php
+include_once("Library/config.php"); 
+
+$uid=$_SESSION['UID'];
+$eid=intval($_GET["eid"]);
+$currentTime=new DateTime();
+
+$result=mysqli_query("SELECT * FROM user_exam_time WHERE uid=$uid and eid=$eid");
+if(mysqli_num_rows($result)){
+	$resultRow=mysqli_fetch_array($result);
+}
+
+?>
