f8ef3aea2b6c7fb6a5f1f473178a832d5a3ee01a
unknown <str@.(none)>
2013-09-08 02:33:55 +0200

    edit2

--- eccp-members/examTimeCheker.php
+++ eccp-members/examTimeCheker.php
@@ -1,15 +1,18 @@
 <?php
-include_once("Library/config.php"); 
+include_once ("Library/config.php");
 
-$uid=$_SESSION['UID'];
-$eid=intval($_GET["eid"]);
-$currentTime=new DateTime();
+$uid = $_SESSION['UID'];
+$eid = intval($_GET["eid"]);
+$currentTime = new DateTime();
 
-$result=mysqli_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
-if(mysqli_num_rows($result)){
-	$resultRow=mysqli_fetch_array($result);
-	$examTimeResult=mysqli_query("SELECT exam_period FROM exam WHERE eid=$eid");
-	$examTimeResultRow=$examTimeResult($$examTimeResult);
+$UETresult = mysqli_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
+if (mysqli_num_rows($result)) {
+	$UETRow = mysqli_fetch_array($UETresult);
+	$examTimeResult = mysqli_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	$examTimeResultRow = mysqli_fetch_array($examTimeResult);
+	$timeLeft = $examTimeResultRow[0] + $UETRow[0] - $currentTime;
+	echo $timeLeft;
+} else {
+mysqli_query
 }
-
 ?>
