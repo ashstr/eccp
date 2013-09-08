6528d44bdb3b5074a14c48afd9a07f1167ca42d3
unknown <str@.(none)>
2013-09-08 02:16:14 +0200

    edit

--- eccp-members/examTimeCheker.php
+++ eccp-members/examTimeCheker.php
@@ -5,9 +5,11 @@ $uid=$_SESSION['UID'];
 $eid=intval($_GET["eid"]);
 $currentTime=new DateTime();
 
-$result=mysqli_query("SELECT * FROM user_exam_time WHERE uid=$uid and eid=$eid");
+$result=mysqli_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
 if(mysqli_num_rows($result)){
 	$resultRow=mysqli_fetch_array($result);
+	$examTimeResult=mysqli_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	$examTimeResultRow=$examTimeResult($$examTimeResult);
 }
 
 ?>
