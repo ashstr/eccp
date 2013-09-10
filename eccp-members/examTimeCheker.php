f292b2e840c161e169210106b4f1c78fa06ba5dc
unknown <str@.(none)>
2013-09-10 17:03:59 +0200

    testing timer v2

--- eccp-members/examTimeCheker.php
+++ eccp-members/examTimeCheker.php
@@ -1,21 +1,21 @@
 <?php
-include_once ("Library/config.php");
-
-$uid = $_SESSION['UID'];
-$eid = intval($_GET["eid"]);
-$currentTime = new DateTime();
-
-$UETresult = mysqli_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
-if (mysqli_num_rows($result)) {
-	$UETRow = mysqli_fetch_array($UETresult);
-	$examTimeResult = mysqli_query("SELECT exam_period FROM exam WHERE eid=$eid");
-	$examTimeResultRow = mysqli_fetch_array($examTimeResult);
-	$timeLeft = $examTimeResultRow[0] + $UETRow[0] - $currentTime;
-	echo $timeLeft;
-} else {
-	mysqli_query("insert into starting_time (uid,eid,starting_time) values ($uid,$eid,$currentTime)");
-	$examTimeResult = mysqli_query("SELECT exam_period FROM exam WHERE eid=$eid");
-	$examTimeResultRow = mysqli_fetch_array($examTimeResult);
-	echo $examTimeResultRow[0];
-}
+phpinfo();
+// $UETresult = mysql_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
+// echo "<script>alert(".mysql_num_rows($UETresult).")</script>";
+// if (mysql_num_rows($UETresult)!=0) {
+	// $UETRow = mysql_fetch_array($UETresult);
+	// $examTimeResult = mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	// $examTimeResultRow = mysql_fetch_array($examTimeResult);
+	// // $timeLeft = $examTimeResultRow[0] + $UETRow[0] - $currentTime;	
+// } else {
+	// echo "<script>alert('$currentTime')</script>";
+	// mysql_query("insert into starting_time (uid,eid,starting_time) values ($uid,$eid,$currentTime)");
+	// echo "<script>alert('after insert')</script>";
+	// $examTimeResult = mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	// $examTimeResultRow = mysql_fetch_array($examTimeResult);
+	// $timeLeft=$examTimeResultRow[0];
+	// echo "<script>alert(".$timeLeft.")</script>";
+// 	
+// }
+// echo $timeLeft;
 ?>
