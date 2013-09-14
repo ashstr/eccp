ae8618cb6a07932fb050a06eed75675f7ace9af2
unknown <str@.(none)>
2013-09-14 02:13:33 +0200

    testing timer loginc

--- eccp-members/examTimeCheker.php
+++ eccp-members/examTimeCheker.php
@@ -1,21 +1,22 @@
 <?php
-phpinfo();
-// $UETresult = mysql_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
-// echo "<script>alert(".mysql_num_rows($UETresult).")</script>";
-// if (mysql_num_rows($UETresult)!=0) {
-	// $UETRow = mysql_fetch_array($UETresult);
-	// $examTimeResult = mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
-	// $examTimeResultRow = mysql_fetch_array($examTimeResult);
-	// // $timeLeft = $examTimeResultRow[0] + $UETRow[0] - $currentTime;	
-// } else {
-	// echo "<script>alert('$currentTime')</script>";
-	// mysql_query("insert into starting_time (uid,eid,starting_time) values ($uid,$eid,$currentTime)");
-	// echo "<script>alert('after insert')</script>";
-	// $examTimeResult = mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
-	// $examTimeResultRow = mysql_fetch_array($examTimeResult);
-	// $timeLeft=$examTimeResultRow[0];
-	// echo "<script>alert(".$timeLeft.")</script>";
-// 	
-// }
-// echo $timeLeft;
+
+$uid = $_SESSION["UID"];
+$eid = $_SESSION["eid"];
+
+$local = localtime();
+$local =$local[0]+$local[1]*60 +$local[2]*3600;
+
+$UETresult = mysql_query("SELECT starting_time FROM user_exam_time WHERE uid=$uid and eid=$eid");
+if (mysql_num_rows($UETresult) != 0) {
+	$UETRow = mysql_fetch_array($UETresult);
+	$examTime= mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	$examTime = mysql_fetch_array($examTime);
+	$timeLeft = $examTime[0]*60 + $UETRow[0] - $local;
+} else {
+	mysql_query("insert into starting_time (uid,eid,starting_time) values ($uid,$eid,$local)");
+	$examTime = mysql_query("SELECT exam_period FROM exam WHERE eid=$eid");
+	$examTime = mysql_fetch_array($examTime);
+	$timeLeft = $examTime[0];
+}
+ echo $timeLeft;
 ?>
